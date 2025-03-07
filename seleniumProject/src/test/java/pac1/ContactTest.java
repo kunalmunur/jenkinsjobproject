6b87159318113eeda6ab978d7b71d6e60123ed4e
kunalmunur <sinhakunalsinha41@gmail.com>
2025-03-07 11:40:24 +0530

    ccccc

---
+++ seleniumProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,27 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContactTest() {
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser");
+		String userName = System.getProperty("username");
+		String password = System.getProperty("password");
+		
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(userName);
+		System.out.println(password);
+		
+		System.out.println("execute createContactTest");
+	}
+     @Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+
+}
---
+++ seleniumProject/src/test/java/pac2/copy/orgTest.java
@@ -0,0 +1,16 @@
+package pac2.copy;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrgTest");
+	}
+     @Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
